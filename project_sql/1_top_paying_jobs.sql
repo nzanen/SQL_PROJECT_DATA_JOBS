3f883f8e547cc215e04ef6499f0711803ba3237e
Niels Zanen <nzanen@gmail.com>
2024-08-13 22:19:36 +0200

    upload sql files

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,24 @@
+/*
+Question: What are the top-paying data analyst jobs?
+- Identify the top 10 highest-paying Data Analyst roles that are availabe remotely
+- Focus on job postings with specified salaries (remove nulls)
+- Why? Highlight the top-paying opportunities for Data Analysts
+*/
+
+SELECT
+    job_id,
+    job_title,
+    name AS company_name,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM 
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location LIKE '%Netherlands%' AND
+    salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC
+LIMIT 10
---
+++ project_sql/2_top_paying_jobs_skills.sql
@@ -0,0 +1,27 @@
+/*
+What skills are required for the top paying data analyst jobs?
+*/
+
+WITH top_paying_jobs AS (
+    SELECT
+        job_id,
+        job_title,
+        name AS company_name,
+        job_location,
+        salary_year_avg
+    FROM 
+        job_postings_fact
+    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE
+        job_title_short = 'Data Analyst' AND
+        job_location LIKE '%Netherlands%' AND
+        salary_year_avg IS NOT NULL
+    ORDER BY salary_year_avg DESC)
+
+SELECT 
+    top_paying_jobs.*,
+    skills_dim.skills
+FROM top_paying_jobs
+INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY salary_year_avg DESC
